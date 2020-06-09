82d5b820f6bf3b9a5acca11b6564c763d89444ce
serhii.shramko <shramko.web@yahoo.com>
2020-06-09 07:32:06 +0300

    implement server cli

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -6,14 +6,15 @@ module.exports = {
   name: `--help`,
   run() {
     const text = `
-    Программа запускает http-сервер и формирует файл с данными для API.
-
+    Программа запускает http-сервер и формирует файл с данными для api.
     Гайд:
-    service.js <command>
-    Команды:
-    --version:            выводит номер версии
-    --help:               печатает этот текст
-    --generate <count>    формирует файл mocks.json
+      service <command>
+
+      Команды:
+      --server <port>       запускает сервер на указанном порте
+      --version:            выводит номер версии
+      --help:               печатает этот текст
+      --generate <count>    формирует файл mocks.json
     `;
 
     console.log(chalk.gray(text));
